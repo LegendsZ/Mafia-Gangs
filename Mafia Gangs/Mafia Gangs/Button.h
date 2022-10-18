4652fd7dd6bc6169d53a29c0261d3ebfb49fd09d
LegendsZ <akkshai@yahoo.com>
2022-10-18 16:02:28 -0400

    working on menu

---
+++ Mafia Gangs/Mafia Gangs/Button.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Rect.h"
+#include "Text.h"
+
+class Button {
+public:
+	Button(int w, int h, int x, int y, int r, int g, int b, int a, SDL_Renderer* renderer, const std::string& font_path, int font_size, const std::string& message, const SDL_Color& color, void(*handler)(SDL_Event&));
+	Button(int w, int h, int x, int y, const std::string& img_path, SDL_Renderer* renderer, const std::string& font_path, int font_size, const std::string& message, const SDL_Color& color, void(*handler)(SDL_Event&));
+	Button(Rect* rect, Text* text, void(*handler)(SDL_Event&));
+
+	bool pollEvents();
+	bool pollEvents(SDL_Event event);
+
+	bool setPos(int x, int y);
+	int* getPos();
+
+
+	bool draw();
+
+	Rect* m_Rect;
+	Text* m_Text;
+
+	void (*m_Handler)(SDL_Event&);
+private:
+};
--- Mafia Gangs/Mafia Gangs/Source.cpp
+++ Mafia Gangs/Mafia Gangs/Source.cpp
@@ -2,21 +2,34 @@
 #undef main
 #include <iostream>
 #include "Window.h"
-#include "Rect.h"
+#include "Button.h"
 
 int main() {
 	system("title MAFIA GANG BOOT");
 
 	//std::cout << "Initializing...";
 	//init vars here
-	const unsigned int screenSizeX = 500, screenSizeY = 500;
+	const unsigned int screenSizeX = 700, screenSizeY = 500;
+	SDL_Event* event = new SDL_Event;
 	Window* mainWindow = new Window("Mafia Gangs | Menu", screenSizeX, screenSizeY);
-	Rect* bkgdMenu = new Rect(screenSizeX, screenSizeY,0,0,50,50,50,1);
+	Rect* bkgdMenu = new Rect(screenSizeX, screenSizeY,0,0,"res/bkgdMenu.jpg");
+	Button* btnPlay = new Button(
+		new Rect(100, 50, (screenSizeX / 2) - (100 / 2), 50, 255, 255, 255, 0),
+		new Text(mainWindow->renderer, "res/comic.ttf", 20, "PLAY", { 0,0,255,1 }),
+		nullptr
+	);
+	Text* title = new Text(mainWindow->renderer, "res/comic.ttf", 10, "MAFIA GANGS", { 255,255,255,1 });
+	title->setPos(0, 0);
 	//std::cout << "OK!\n";
 	//system("pause");
 	while (!mainWindow->isClosed()) {
 		SDL_ShowWindow(mainWindow->m_Window);
+
+		SDL_PollEvent(event);
+
 		bkgdMenu->draw();
+		btnPlay->draw();
+		title->display();
 		mainWindow->clear();
 	}
 
